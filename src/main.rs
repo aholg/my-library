75e95d2582dcffe823507d2d006d258287952ae0
aholg <holgersson31@hotmail.com>
2018-06-04 20:37:50 +0200

    Add serving of simple html page.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 extern crate iron;
+extern crate mount;
+extern crate staticfile;
 
-use iron::Request;
 use iron::prelude::*;
-use iron::status;
+use std::path::Path;
+use mount::Mount;
+use staticfile::Static;
 
 fn main() {
-    Iron::new(|_: &mut Request| {
-        Ok(Response::with((status::Ok, "Hello World!")))
-    }).http("localhost:3000").unwrap();
+    let mut mount = Mount::new();
+
+    mount.mount("/index", Static::new(Path::new("src/html/index.html")));
+
+    Iron::new(mount).http("localhost:3000").unwrap();
 }
