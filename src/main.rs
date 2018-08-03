18bbd5448b5bff151a6b8e120d3e436936de2a97
Anton Holgersson <holgersson31@hotmail.com>
2018-08-03 12:30:34 +0200

    Add templating with handlebars engine

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,37 @@
+extern crate handlebars_iron as hbs;
 extern crate iron;
-extern crate mount;
-extern crate staticfile;
+extern crate router;
 
 use iron::prelude::*;
-use std::path::Path;
-use mount::Mount;
-use staticfile::Static;
+use iron::status;
+use router::Router;
+use hbs::HandlebarsEngine;
 
 fn main() {
-    let mut mount = Mount::new();
+    let template_engine = setup_templates();
+    setup_routes(template_engine);
+}
 
-    mount.mount("/index", Static::new(Path::new("src/html/index.html")));
+fn setup_templates() -> HandlebarsEngine {
+    let mut templateEngine = hbs::HandlebarsEngine::new();
+    templateEngine.add(Box::new(hbs::DirectorySource::new("./src/html/", ".hbs")));
+    if let Err(r) = templateEngine.reload() {
+        panic!("{}", r);
+    }
+    templateEngine
+}
 
-    Iron::new(mount).http("localhost:3000").unwrap();
+fn setup_routes(mut templateEngine: HandlebarsEngine) {
+    let mut router = Router::new();
+    router.get("/index", index_handler, "index");
+
+    let mut chain = Chain::new(router);
+    chain.link_after(templateEngine);
+    Iron::new(chain).http("localhost:3000").unwrap();
+}
+
+fn index_handler(_: &mut Request) -> IronResult<Response> {
+    let mut resp = Response::new();
+    resp.set_mut(hbs::Template::new("index", "")).set_mut(status::Ok);
+    Ok(resp)
 }
