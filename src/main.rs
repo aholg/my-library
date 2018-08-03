426c9933a6345f6de66b474fae9e189424f08d1e
Anton Holgersson <holgersson31@hotmail.com>
2018-08-03 14:27:26 +0200

    Add serialization of a link class

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 extern crate handlebars_iron as hbs;
 extern crate iron;
 extern crate router;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
 
 use iron::prelude::*;
 use iron::status;
 use router::Router;
 use hbs::HandlebarsEngine;
+use hbs::Template;
+use hbs::handlebars::to_json;
 
 fn main() {
     let template_engine = setup_templates();
@@ -13,25 +18,33 @@ fn main() {
 }
 
 fn setup_templates() -> HandlebarsEngine {
-    let mut templateEngine = hbs::HandlebarsEngine::new();
-    templateEngine.add(Box::new(hbs::DirectorySource::new("./src/html/", ".hbs")));
-    if let Err(r) = templateEngine.reload() {
+    let mut template_engine = hbs::HandlebarsEngine::new();
+    template_engine.add(Box::new(hbs::DirectorySource::new("./src/html/", ".hbs")));
+    if let Err(r) = template_engine.reload() {
         panic!("{}", r);
     }
-    templateEngine
+    template_engine
 }
 
-fn setup_routes(mut templateEngine: HandlebarsEngine) {
+fn setup_routes(template_engine: HandlebarsEngine) {
     let mut router = Router::new();
     router.get("/index", index_handler, "index");
 
     let mut chain = Chain::new(router);
-    chain.link_after(templateEngine);
+    chain.link_after(template_engine);
     Iron::new(chain).http("localhost:3000").unwrap();
 }
 
 fn index_handler(_: &mut Request) -> IronResult<Response> {
+    let link = Link { link: "https://i.pinimg.com/originals/fe/77/26/fe77263d6fa1ec9a262a186789d5a4e6.gif".to_string() };
+    let data = to_json(&link);
+
     let mut resp = Response::new();
-    resp.set_mut(hbs::Template::new("index", "")).set_mut(status::Ok);
+    resp.set_mut(Template::new("index", data)).set_mut(status::Ok);
     Ok(resp)
+}
+
+#[derive(Serialize, Debug)]
+pub struct Link {
+    link: String,
 }
